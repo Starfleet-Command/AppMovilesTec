04965045a5a3f4ee8d0a97b38b32335c22fa3bcc
Starfleet-Command <juanfgort@gmail.com>
2020-03-02 21:21:03 -0600

    Clase 02/03/20

--- screens/Deals.js
+++ screens/Deals.js
@@ -1,36 +1,54 @@
 import React, { Component } from "react";
-import { Container, Header, Content, Card, CardItem, Text, Body, Footer, FooterTab, } from "native-base";
-import { View, Image } from 'react-native';
+import { Container, Header, Content, Card, CardItem, Text, Body, Footer, FooterTab, Thumbnail, } from "native-base";
+import { View, Image, FlatList } from 'react-native';
 
 
 export default class CardItemBordered extends Component {
+    constructor(props) {
+        super(props);
+
+        global.getProducts = [];
+
+        this.state = {
+            getProducts: [],
+        };
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/carddata')
+            .then(response => response.json())
+            .then(responseJson => {
+                this.setState({ getProducts: responseJson })
+                console.log(responseJson)
+            })
+            .catch(error => console.log(error));
+    }
+
     render() {
         return (
-            <Container>
-                <Header />
-                <Content padder>
-                    <Card>
-                        <CardItem header bordered>
-                            <Text>$950</Text>
-                        </CardItem>
-                        <CardItem bordered>
-                            <Body>
-                                <Text>
-                                    The Last of Us II
-                                </Text>
+            <View>
+                <FlatList
+                    vertical={true}
+                    data={this.state.getProducts}
+                    renderItem={({ item }) => (
 
-                                <Image
-                                    style={{ width: 150, height: 200 }}
-                                    source={{ uri: 'https://cdn.wccftech.com/wp-content/uploads/2019/08/WCCFthelastofuspart23-740x429.jpg' }}
-                                />
-                            </Body>
-                        </CardItem>
-                        <CardItem footer bordered button onPress={() => alert("PH for link to store")}>
-                            <Text>PS Store</Text>
-                        </CardItem>
-                    </Card>
-                </Content>
-            </Container>
+                        <Card>
+                            <CardItem>
+                                <Body>
+                                    <Text style={{ fontWeight: "bold" }}>
+                                        {item.type}
+                                    </Text>
+                                    <Thumbnail
+                                        style={{ height: 300, width: 370 }}
+                                        source={{ uri: item.image }}
+                                    />
+                                </Body>
+                            </CardItem>
+                        </Card>
+                    )}
+                />
+            </View>
         );
     }
 }
+
--- screens/Scryfall.js
+++ screens/Scryfall.js
@@ -1,43 +1,48 @@
-import React, { Component } from "react";
-import { Container, Header, Content, Card, CardItem, Text, Body, Footer, FooterTab, } from "native-base";
+import React, { Component } from 'react';
+import {
+    Container,
+    Header,
+    Content,
+    Card,
+    CardItem,
+    Text,
+    Body,
+    Footer,
+    FooterTab,
+} from 'native-base';
 import { View, Image, TextInput, TouchableOpacity, Linking } from 'react-native';
 import styles, { IMAGE_HEIGHT, IMAGE_HEIGHT_SMALL } from './styles';
-
-
 
 export default class CardItemBordered extends Component {
     constructor(props) {
         super(props);
     }
 
-
     state = {
         card: 'Decimate',
         uri: 'https://api.scryfall.com/cards/named?fuzzy=Aus+com',
         image_uri: 'none',
         price: ' ',
-        store_uri: ''
+        store_uri: '',
     };
 
     async getCardsFromApiAsync() {
         return fetch(this.state.uri)
-            .then((response) => response.json())
-            .then((responseJson) => {
-
-                this.setState({
-                    image_uri: responseJson.image_uris.small,
-                    price: responseJson.prices.usd,
-                    store_uri: responseJson.purchase_uris.tcgplayer
-                }, function () {
-
-                });
-
+            .then(response => response.json())
+            .then(responseJson => {
+                this.setState(
+                    {
+                        image_uri: responseJson.image_uris.small,
+                        price: responseJson.prices.usd,
+                        store_uri: responseJson.purchase_uris.tcgplayer,
+                    },
+                    function () { },
+                );
             })
-            .catch((error) => {
+            .catch(error => {
                 console.error(error);
             });
     }
-
 
     render() {
         return (
@@ -45,15 +50,17 @@ export default class CardItemBordered extends Component {
                 <Header />
                 <Content padder>
                     <View style={styles.container}>
-
-
                         <TextInput
-                            placeholder='Type the card here'
-
-                            onChangeText={(card) => this.setState({ card, uri: 'https://api.scryfall.com/cards/named?fuzzy=' + this.state.card })}
+                            placeholder="Type the card here"
+                            onChangeText={card =>
+                                this.setState({
+                                    card,
+                                    uri:
+                                        'https://api.scryfall.com/cards/named?fuzzy=' +
+                                        this.state.card,
+                                })
+                            }
                             value={this.state.card}
-
-
                         />
                     </View>
 
@@ -61,17 +68,17 @@ export default class CardItemBordered extends Component {
                         <View style={styles.button}>
                             <Text
                                 button
-                                onPress={() =>
-                                    this.getCardsFromApiAsync()
-                                }
+                                onPress={() => this.getCardsFromApiAsync()}
                                 style={styles.buttonText}>
                                 Search
-                </Text>
+              </Text>
                         </View>
                     </TouchableOpacity>
                     <Card>
                         <CardItem header bordered>
-                            <Text>{this.state.card}: ${this.state.price}</Text>
+                            <Text>
+                                {this.state.card}: ${this.state.price}
+                            </Text>
                         </CardItem>
                         <CardItem bordered>
                             <Body>
@@ -81,7 +88,11 @@ export default class CardItemBordered extends Component {
                                 />
                             </Body>
                         </CardItem>
-                        <CardItem footer bordered button onPress={() => Linking.openURL(this.state.store_uri)}>
+                        <CardItem
+                            footer
+                            bordered
+                            button
+                            onPress={() => Linking.openURL(this.state.store_uri)}>
                             <Text>Buy on TcgPlayer</Text>
                         </CardItem>
                     </Card>
